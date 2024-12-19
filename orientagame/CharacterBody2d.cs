75a7f31fc4230bdf4330e9a640f2756f2074a23b
Gianni Navolio <navolio2000@gmail.com>
2024-12-19 11:37:10 +0100

    Implementato il sistema di collisione del personaggio

--- orientagame/CharacterBody2d.cs
+++ orientagame/CharacterBody2d.cs
@@ -4,37 +4,63 @@ using System;
 public partial class CharacterBody2d : CharacterBody2D
 {
 	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	private string lastDirection = "down";
+	private Vector2 ScreenSize;
+	
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
+		MoveAndCollide(velocity);
+	}
 
-		// Add the gravity.
-		if (!IsOnFloor())
+	public override void _Ready()
+	{
+		ScreenSize = GetViewportRect().Size;
+		GD.Print($"ScreenSize: {ScreenSize}");
+	}
+
+	public override void _Process(double delta)
+	{
+		var velocity = new Vector2(); // The player's movement vector
+
+		if(Input.IsActionPressed("move_right"))
 		{
-			velocity += GetGravity() * (float)delta;
+			velocity.X += 1;
+			lastDirection = "right";
+		}
+		if(Input.IsActionPressed("move_left"))
+		{
+			velocity.X -= 1;
+			lastDirection = "left";
+		}
+		if(Input.IsActionPressed("move_down"))
+		{
+			velocity.Y += 1;
+			lastDirection = "down";
+		}
+		if(Input.IsActionPressed("move_up"))
+		{
+			velocity.Y -= 1;
+			lastDirection = "up";
 		}
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
+	if (velocity.Length() > 0)
 		{
-			velocity.X = direction.X * Speed;
+			velocity = velocity.Normalized() * Speed;
+
+			// Gioca l'animazione di movimento in base alla direzione
+			animatedSprite2D.Play("move_" + lastDirection);
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			// Gioca l'animazione "idle" basata sull'ultima direzione
+			animatedSprite2D.Play("idel_" + lastDirection);
 		}
 
-		Velocity = velocity;
-		MoveAndSlide();
+		// Aggiorna la posizione del personaggio
+		Position += velocity * (float)delta;
 	}
 }
